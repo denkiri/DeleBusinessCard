e2df96d4782de62a5780e7756f2d20ccfd1204eb
denkiri <denniskirimi.dk@gmail.com>
2024-02-22 12:36:43 +0300

    Initial Commit

---
+++ Dele Card/Dele_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dele_CardApp.swift
+//  Dele Card
+//
+//  Created by Macbook Pro on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Dele_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
