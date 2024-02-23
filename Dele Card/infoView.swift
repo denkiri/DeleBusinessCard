8c235f325c220f7c371e9a665ebfd9254cb7a01a
denkiri <denniskirimi.dk@gmail.com>
2024-02-23 14:49:47 +0300

    Initial Commit

---
+++ Dele Card/infoView.swift
@@ -0,0 +1,18 @@
+//
+//  infoView.swift
+//  Dele Card
+//
+//  Created by Macbook Pro on 23/02/2024.
+//
+
+import SwiftUI
+
+struct infoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    infoView()
+}
